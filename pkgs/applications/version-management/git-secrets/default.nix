ad6e500adc4762d56546b87f043e2258be1aeb1d
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:41:48 +0000

    git-secrets: use finalAttrs

--- pkgs/applications/version-management/git-secrets/default.nix
+++ pkgs/applications/version-management/git-secrets/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, makeWrapper, git, coreutils }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "git-secrets";
   version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "git-secrets";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "10lnxg0q855zi3d6804ivlrn6dc817kilzdh05mmz8a0ccvm2qc7";
   };
 
@@ -30,4 +30,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.all;
     mainProgram = "git-secrets";
   };
-}
+})
