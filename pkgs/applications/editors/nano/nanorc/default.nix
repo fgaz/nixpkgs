4c53cbbdc9647367c73848139b9dc2b76e53d41c
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:14:48 +0000

    nanorc: use finalAttrs

--- pkgs/applications/editors/nano/nanorc/default.nix
+++ pkgs/applications/editors/nano/nanorc/default.nix
@@ -4,13 +4,13 @@
 let
   owner = "scopatz";
   repo = "nanorc";
-in stdenv.mkDerivation rec {
+in stdenv.mkDerivation (finalAttrs: {
   pname = "nanorc";
   version = "2020-10-10";
 
   src = fetchFromGitHub {
     inherit owner repo;
-    rev = builtins.replaceStrings [ "-" ] [ "." ] version;
+    rev = builtins.replaceStrings [ "-" ] [ "." ] finalAttrs.version;
     sha256 = "3B2nNFYkwYHCX6pQz/hMO/rnVqlCiw1BSNmGmJ6KCqE=";
   };
 
@@ -34,15 +34,15 @@ in stdenv.mkDerivation rec {
         nix
       ]
     }
-    oldVersion="$(nix-instantiate --eval -E "with import ./. {}; lib.getVersion ${pname}" | tr -d '"' | sed 's|\\.|-|g')"
-    latestTag="$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags git@github.com:${owner}/${repo} '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3)"
+    oldVersion="$(nix-instantiate --eval -E "with import ./. {}; lib.getVersion ${finalAttrs.pname}" | tr -d '"' | sed 's|\\.|-|g')"
+    latestTag="$(git -c 'finalAttrs.versionsort.suffix=-' ls-remote --exit-code --refs --sort='finalAttrs.version:refname' --tags git@github.com:${owner}/${repo} '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3)"
     if [ "$oldVersion" != "$latestTag" ]; then
       nixpkgs="$(git rev-parse --show-toplevel)"
       default_nix="$nixpkgs/pkgs/applications/editors/nano/nanorc/default.nix"
       newTag=$(echo $latestTag | sed 's|\.|-|g')
-      update-source-version ${pname} "$newTag" --version-key=version --print-changes
+      update-source-finalAttrs.version ${finalAttrs.pname} "$newTag" --finalAttrs.version-key=finalAttrs.version --print-changes
     else
-      echo "${pname} is already up-to-date"
+      echo "${finalAttrs.pname} is already up-to-date"
     fi
   '';
 
@@ -53,4 +53,4 @@ in stdenv.mkDerivation rec {
     maintainers = with lib.maintainers; [ nequissimus ];
     platforms = lib.platforms.all;
   };
-}
+})
