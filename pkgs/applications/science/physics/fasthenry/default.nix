bfd8ff34482604a892a82ead41fce37976ec3f49
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:41:29 +0000

    fasthenry: use finalAttrs

--- pkgs/applications/science/physics/fasthenry/default.nix
+++ pkgs/applications/science/physics/fasthenry/default.nix
@@ -3,20 +3,20 @@
 , lib
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "fasthenry";
-  # later versions are Windows only ports
+  # later finalAttrs.versions are Windows only ports
   # nixpkgs-update: no auto update
   version = "3.0.1";
 
   # we don't use the original MIT code at
   # https://www.rle.mit.edu/cpg/research_codes.htm
-  # since the FastFieldSolvers S.R.L. version includes
+  # since the FastFieldSolvers S.R.L. finalAttrs.version includes
   # a couple of bug fixes
   src = fetchFromGitHub {
     owner = "ediloren";
     repo = "FastHenry2";
-    rev = "R${version}";
+    rev = "R${finalAttrs.version}";
     sha256 = "017kcri69zhyhii59kxj1ak0gyfn7jf0qp6p2x3nnljia8njdkcc";
   };
 
@@ -42,10 +42,10 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bin
     cp -r bin/* $out/bin/
-    mkdir -p $out/share/doc/${pname}-${version}
-    cp -r doc/* $out/share/doc/${pname}-${version}
-    mkdir -p $out/share/${pname}-${version}/examples
-    cp -r examples/* $out/share/${pname}-${version}/examples
+    mkdir -p $out/share/doc/${finalAttrs.pname}-${finalAttrs.version}
+    cp -r doc/* $out/share/doc/${finalAttrs.pname}-${finalAttrs.version}
+    mkdir -p $out/share/${finalAttrs.pname}-${finalAttrs.version}/examples
+    cp -r examples/* $out/share/${finalAttrs.pname}-${finalAttrs.version}/examples
   '';
 
   meta = with lib; {
@@ -58,4 +58,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ fbeffa ];
     platforms = intersectLists (platforms.linux) (platforms.x86_64 ++ platforms.x86);
   };
-}
+})
