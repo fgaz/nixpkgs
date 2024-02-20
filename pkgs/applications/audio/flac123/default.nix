3685185af720a16a253c1a2874b02514e869c139
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:27:27 +0000

    flac123: use finalAttrs

--- pkgs/applications/audio/flac123/default.nix
+++ pkgs/applications/audio/flac123/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl, autoreconfHook, flac, libao, libogg, popt }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "flac123";
   version = "0.0.12";
 
   src = fetchurl {
-    url = "mirror://sourceforge/flac-tools/${pname}-${version}-release.tar.gz";
+    url = "mirror://sourceforge/flac-tools/${finalAttrs.pname}-${finalAttrs.version}-release.tar.gz";
     sha256 = "0zg4ahkg7v81za518x32wldf42g0rrvlrcqhrg9sv3li9bayyxhr";
   };
 
@@ -18,4 +18,4 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2Plus;
     platforms = platforms.all;
   };
-}
+})
