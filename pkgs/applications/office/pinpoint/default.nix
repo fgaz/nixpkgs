05b9463fe5e43ce9758e7d5b547c83f75e779f4d
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:48:37 +0000

    pinpoint: use finalAttrs

--- pkgs/applications/office/pinpoint/default.nix
+++ pkgs/applications/office/pinpoint/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, lib, stdenv, pkg-config, autoconf, automake, clutter, clutter-gst
 , gdk-pixbuf, cairo, clutter-gtk }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "pinpoint";
   version = "0.1.8";
   src = fetchurl {
-    url = "http://ftp.gnome.org/pub/GNOME/sources/pinpoint/0.1/${pname}-${version}.tar.xz";
+    url = "http://ftp.gnome.org/pub/GNOME/sources/pinpoint/0.1/${finalAttrs.pname}-${finalAttrs.version}.tar.xz";
     sha256 = "1jp8chr9vjlpb5lybwp5cg6g90ak5jdzz9baiqkbg0anlg8ps82s";
   };
   nativeBuildInputs = [ pkg-config autoconf automake ];
@@ -19,4 +19,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     maintainers = with maintainers; [ pSub ];
   };
-}
+})
