03e3fd0c557fffa50a398bcdfe2c7d8ea891d7e4
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:44:27 +0000

    xnec2c: use finalAttrs

--- pkgs/applications/science/physics/xnec2c/default.nix
+++ pkgs/applications/science/physics/xnec2c/default.nix
@@ -12,12 +12,12 @@
 
 assert (!blas.isILP64) && (!lapack.isILP64);
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xnec2c";
   version = "4.4.12";
 
   src = fetchurl {
-    url = "https://www.xnec2c.org/releases/${pname}-v${version}.tar.gz";
+    url = "https://www.xnec2c.org/releases/${finalAttrs.pname}-v${finalAttrs.version}.tar.gz";
     hash = "sha256-6Yrx6LkJjfnMA/kJUDWLhGzGopZeecARSrcR++UScsU=";
   };
 
@@ -39,4 +39,4 @@ stdenv.mkDerivation rec {
     # Darwin support likely to be fixed upstream in the next release
     broken = stdenv.isDarwin;
   };
-}
+})
