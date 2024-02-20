85bb8ff519ddc51b3a8b33e10c926bd78a311024
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:50:28 +0000

    gnucap: use finalAttrs

--- pkgs/applications/science/electronics/gnucap/default.nix
+++ pkgs/applications/science/electronics/gnucap/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gnucap";
   version = "20210107";
 
   src = fetchurl {
-    url = "https://git.savannah.gnu.org/cgit/gnucap.git/snapshot/${pname}-${version}.tar.gz";
+    url = "https://git.savannah.gnu.org/cgit/gnucap.git/snapshot/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
     sha256 = "12rlwd4mfc54qq1wrx5k8qk578xls5z4isf94ybkf2z6qxk4mhnj";
   };
 
@@ -18,10 +18,10 @@ Gnucap is a modern general purpose circuit simulator with several advantages ove
 It performs nonlinear dc and transient analyses, fourier analysis, and ac analysis.
     '';
     homepage = "http://www.gnucap.org/";
-    changelog = "https://git.savannah.gnu.org/cgit/gnucap.git/plain/NEWS?h=v${version}";
+    changelog = "https://git.savannah.gnu.org/cgit/gnucap.git/plain/NEWS?h=v${finalAttrs.version}";
     license = licenses.gpl3Plus;
     platforms = platforms.all;
     broken = stdenv.isDarwin; # Relies on LD_LIBRARY_PATH
     maintainers = [ maintainers.raboof ];
   };
-}
+})
