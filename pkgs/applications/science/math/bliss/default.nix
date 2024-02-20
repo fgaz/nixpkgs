9642019a1f9a01d49702f7a1485cffc31d6c4917
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:43:39 +0000

    bliss: use finalAttrs

--- pkgs/applications/science/math/bliss/default.nix
+++ pkgs/applications/science/math/bliss/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl, unzip, doxygen }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "bliss";
   version = "0.73";
 
   src = fetchurl {
-    url = "http://www.tcs.hut.fi/Software/bliss/${pname}-${version}.zip";
+    url = "http://www.tcs.hut.fi/Software/bliss/${finalAttrs.pname}-${finalAttrs.version}.zip";
     sha256 = "f57bf32804140cad58b1240b804e0dbd68f7e6bf67eba8e0c0fa3a62fd7f0f84";
   };
 
@@ -34,4 +34,4 @@ stdenv.mkDerivation rec {
     license = licenses.lgpl3;
     platforms = [ "i686-linux" "x86_64-linux" "aarch64-linux" ];
   };
-}
+})
