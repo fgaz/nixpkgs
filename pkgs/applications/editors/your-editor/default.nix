c26d9ab531e6e685def0f5022a5d667c41445ee8
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:20:52 +0000

    your-editor: use finalAttrs

--- pkgs/applications/editors/your-editor/default.nix
+++ pkgs/applications/editors/your-editor/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "your-editor";
   version = "1506";
 
   src = fetchFromGitHub {
     owner = "your-editor";
     repo = "yed";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "sha256-QmUquXoDGhoan+Y1kdkTirdkIvYPBkeAEkMLkaE9QKk=";
   };
 
@@ -21,10 +21,10 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Your-editor (yed) is a small and simple terminal editor core that is meant to be extended through a powerful plugin architecture";
     homepage = "https://your-editor.org/";
-    changelog = "https://github.com/your-editor/yed/blob/${version}/CHANGELOG.md";
+    changelog = "https://github.com/your-editor/yed/blob/${finalAttrs.version}/CHANGELOG.md";
     license = with licenses; [ mit ];
     platforms = platforms.unix;
     maintainers = with maintainers; [ uniquepointer ];
     mainProgram = "yed";
   };
-}
+})
