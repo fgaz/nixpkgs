3a8e1ddfd62b46348afe82ba9feebb33be80d9c0
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:10:27 +0000

    grb: use finalAttrs

--- pkgs/applications/misc/grb/default.nix
+++ pkgs/applications/misc/grb/default.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "grb";
   version = "unstable-2022-07-02";
 
   src = fetchFromGitHub {
     owner = "LukeSmithxyz";
-    repo = pname;
+    repo = finalAttrs.pname;
     rev = "35a5353ab147b930c39e1ccd369791cc4c27f0df";
     sha256 = "sha256-hQ21DXnkBJVCgGXQKDR+DjaDC3RXS2pNmSLDoHvHA4E=";
   };
@@ -21,4 +21,4 @@ stdenv.mkDerivation rec {
     license = licenses.publicDomain;
     maintainers = [ maintainers.cafkafk ];
   };
-}
+})
