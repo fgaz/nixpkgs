edceaa3dbfac2e5bef4da8e578a42fa219cbb9ca
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:08:10 +0000

    scantailor-universal: use finalAttrs

--- pkgs/applications/graphics/scantailor/universal.nix
+++ pkgs/applications/graphics/scantailor/universal.nix
@@ -15,14 +15,14 @@
 , libcanberra
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "scantailor-universal";
   version = "0.2.14";
 
   src = fetchFromGitHub {
     owner = "trufanov-nok";
-    repo = pname;
-    rev = version;
+    repo = finalAttrs.pname;
+    rev = finalAttrs.version;
     fetchSubmodules = true;
     hash = "sha256-n8NbokK+U0FAuYXtjRJcxlI1XAmI4hk5zV3sF86hB/s=";
   };
@@ -38,4 +38,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.unix;
     mainProgram = "scantailor-universal-cli";
   };
-}
+})
