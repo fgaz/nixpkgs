14b6b4ad20557ccc919453ee88caeebd4dc69785
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:16:16 +0000

    gmt: use finalAttrs

--- pkgs/applications/gis/gmt/default.nix
+++ pkgs/applications/gis/gmt/default.nix
@@ -7,11 +7,11 @@
     - graphicsmagick for gif output
 */
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gmt";
   version = "6.4.0";
   src = fetchurl {
-    url = "https://github.com/GenericMappingTools/gmt/releases/download/${version}/gmt-${version}-src.tar.gz";
+    url = "https://github.com/GenericMappingTools/gmt/releases/download/${finalAttrs.version}/gmt-${finalAttrs.version}-src.tar.gz";
     sha256 = "sha256-0mfAx9b7MMnqfgKe8n2tsm/9e5LLS0cD+aO6Do85Ohs=";
   };
 
@@ -70,4 +70,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ tviti ];
   };
 
-}
+})
