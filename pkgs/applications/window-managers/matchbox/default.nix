88b2721f4ea4e24b182e718ab3bb66bcd0d38c5f
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:09:41 +0000

    matchbox: use finalAttrs

--- pkgs/applications/window-managers/matchbox/default.nix
+++ pkgs/applications/window-managers/matchbox/default.nix
@@ -1,6 +1,6 @@
 { lib, stdenv, fetchurl, fetchpatch, pkg-config, libmatchbox, libX11, libXext }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "matchbox";
   version = "1.2";
 
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
   NIX_LDFLAGS = "-lX11 -L${libX11}/lib -lXext -L${libXext}/lib";
 
   src = fetchurl {
-    url = "https://downloads.yoctoproject.org/releases/matchbox/matchbox-window-manager/${version}/matchbox-window-manager-${version}.tar.bz2";
+    url = "https://downloads.yoctoproject.org/releases/matchbox/matchbox-window-manager/${finalAttrs.version}/matchbox-window-manager-${finalAttrs.version}.tar.bz2";
     sha256 = "1zyfq438b466ygcz78nvsmnsc5bhg4wcfnpxb43kbkwpyx53m8l1";
   };
 
@@ -28,4 +28,4 @@ stdenv.mkDerivation rec {
     license = lib.licenses.gpl2Plus;
     platforms = lib.platforms.linux;
   };
-}
+})
