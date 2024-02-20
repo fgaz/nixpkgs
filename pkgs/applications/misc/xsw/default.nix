a3bb2b1384f15e4a8dc4be07989b256415b3f836
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:05:01 +0000

    xsw: use finalAttrs

--- pkgs/applications/misc/xsw/default.nix
+++ pkgs/applications/misc/xsw/default.nix
@@ -3,14 +3,14 @@
 let
   makeSDLFlags = map (p: "-I${lib.getDev p}/include/SDL");
 
-in stdenv.mkDerivation rec {
+in stdenv.mkDerivation (finalAttrs: {
   pname = "xsw";
   version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "andrenho";
     repo = "xsw";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "092vp61ngd2vscsvyisi7dv6qrk5m1i81gg19hyfl5qvjq5p0p8g";
   };
 
@@ -32,4 +32,4 @@ in stdenv.mkDerivation rec {
     license  = licenses.gpl3;
     maintainers = [ maintainers.vrthra ];
   };
-}
+})
