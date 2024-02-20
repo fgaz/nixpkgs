0583397a5f50bbe0bd3231e86b6b2fd4e7fee37c
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:40:10 +0000

    emuflight-configurator: use finalAttrs

--- pkgs/applications/science/robotics/emuflight-configurator/default.nix
+++ pkgs/applications/science/robotics/emuflight-configurator/default.nix
@@ -1,12 +1,12 @@
 {lib, stdenv, fetchurl, unzip, makeDesktopItem, copyDesktopItems, nwjs
 , wrapGAppsHook, gsettings-desktop-schemas, gtk3 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "emuflight-configurator";
   version = "0.4.1";
 
   src = fetchurl {
-    url = "https://github.com/emuflight/EmuConfigurator/releases/download/${version}/emuflight-configurator_${version}_linux64.zip";
+    url = "https://github.com/emuflight/EmuConfigurator/releases/download/${finalAttrs.version}/emuflight-configurator_${finalAttrs.version}_linux64.zip";
     sha256 = "sha256-e4HNg5yr9V5LyT0hYP6gzw0tZm4dLidJg5MQtH3L3JI=";
   };
 
@@ -15,19 +15,19 @@ stdenv.mkDerivation rec {
   buildInputs = [ gsettings-desktop-schemas gtk3 ];
 
   installPhase = ''
-    mkdir -p $out/bin $out/share/${pname}
+    mkdir -p $out/bin $out/share/${finalAttrs.pname}
 
-    cp -r . $out/share/${pname}/
-    install -m 444 -D icon/emu_icon_128.png $out/share/icons/hicolor/128x128/apps/${pname}.png
+    cp -r . $out/share/${finalAttrs.pname}/
+    install -m 444 -D icon/emu_icon_128.png $out/share/icons/hicolor/128x128/apps/${finalAttrs.pname}.png
 
-    makeWrapper ${nwjs}/bin/nw $out/bin/${pname} --add-flags $out/share/${pname}
+    makeWrapper ${nwjs}/bin/nw $out/bin/${finalAttrs.pname} --add-flags $out/share/${finalAttrs.pname}
   '';
 
   desktopItems = [
     (makeDesktopItem {
-      name = pname;
-      exec = pname;
-      icon = pname;
+      name = finalAttrs.pname;
+      exec = finalAttrs.pname;
+      icon = finalAttrs.pname;
       comment = "Emuflight configuration tool";
       desktopName = "Emuflight Configurator";
       genericName = "Flight controller configuration tool";
@@ -48,4 +48,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ beezow ];
     platforms   = platforms.linux;
   };
-}
+})
