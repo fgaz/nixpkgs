b0c8f73ca654dc9215704c9e88cb8dc438c2957e
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:53:40 +0000

    xearth: use finalAttrs

--- pkgs/applications/science/astronomy/xearth/default.nix
+++ pkgs/applications/science/astronomy/xearth/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl, imake, gccmakedep, libXt, libXext }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xearth";
   version = "1.1";
 
   src = fetchurl {
-    url = "http://xearth.org/${pname}-${version}.tar.gz";
+    url = "http://xearth.org/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
     sha256 = "bcb1407cc35b3f6dd3606b2c6072273b6a912cbd9ed1ae22fb2d26694541309c";
   };
 
@@ -32,4 +32,4 @@ stdenv.mkDerivation rec {
     platforms=platforms.unix;
   };
 
-}
+})
