48093a2e4084a81fbc9baa5b160ad878f14fc028
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:16:08 +0000

    xmountains: use finalAttrs

--- pkgs/applications/graphics/xmountains/default.nix
+++ pkgs/applications/graphics/xmountains/default.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchFromGitHub, xorg }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xmountains";
   version = "2.10";
 
   src = fetchFromGitHub {
     owner = "spbooth";
-    repo = pname;
+    repo = finalAttrs.pname;
     rev = "aa3bcbfed228adf3fff0fe4295589f13fc194f0b";
     sha256 = "0dx4n2y736lv04sj41cp1dw8n5zkw5gyd946a6zsiv0k796s9ra9";
   };
@@ -22,4 +22,4 @@ stdenv.mkDerivation rec {
     license = licenses.hpndSellVariant;
     maintainers = with maintainers; [ djanatyn ];
   };
-}
+})
