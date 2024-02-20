ed7738da3561d80e6c1e4c6ab70a30d5639caa5d
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:54:52 +0000

    rssguard: use finalAttrs

--- pkgs/applications/networking/feedreaders/rssguard/default.nix
+++ pkgs/applications/networking/feedreaders/rssguard/default.nix
@@ -8,14 +8,14 @@
 , wrapQtAppsHook
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "rssguard";
   version = "4.5.3";
 
   src = fetchFromGitHub {
     owner = "martinrotter";
-    repo = pname;
-    rev = "refs/tags/${version}";
+    repo = finalAttrs.pname;
+    rev = "refs/tags/${finalAttrs.version}";
     sha256 = "sha256-eF0jPT0gQnnBWu9IKfY0DwMwotL3IEjovqnQqx9v2NA=";
   };
 
@@ -31,9 +31,9 @@ stdenv.mkDerivation rec {
       for ownCloud/Nextcloud.
     '';
     homepage = "https://github.com/martinrotter/rssguard";
-    changelog = "https://github.com/martinrotter/rssguard/releases/tag/${version}";
+    changelog = "https://github.com/martinrotter/rssguard/releases/tag/${finalAttrs.version}";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ jluttine ];
   };
-}
+})
