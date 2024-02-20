f16b6f6e9f2aff2c566df4d74e6faec880920a64
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:10:30 +0000

    clipmenu: use finalAttrs

--- pkgs/applications/misc/clipmenu/default.nix
+++ pkgs/applications/misc/clipmenu/default.nix
@@ -10,14 +10,14 @@
 , xdotool
 , xsel
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "clipmenu";
   version = "6.2.0";
 
   src = fetchFromGitHub {
     owner  = "cdown";
     repo   = "clipmenu";
-    rev    = version;
+    rev    = finalAttrs.version;
     sha256 = "sha256-nvctEwyho6kl4+NXi76jT2kG7nchmI2a7mgxlgjXA5A=";
   };
 
@@ -45,4 +45,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ jb55 ];
     license = licenses.publicDomain;
   };
-}
+})
