3c8f9b4feb54f76155c42d4073115f951b5a7bd9
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:58:15 +0000

    iprange: use finalAttrs

--- pkgs/applications/networking/firehol/iprange.nix
+++ pkgs/applications/networking/firehol/iprange.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "iprange";
   version = "1.0.4";
 
   src = fetchurl {
-    url = "https://github.com/firehol/iprange/releases/download/v${version}/iprange-${version}.tar.xz";
+    url = "https://github.com/firehol/iprange/releases/download/v${finalAttrs.version}/iprange-${finalAttrs.version}.tar.xz";
     sha256 = "0rymw4ydn09dng34q4g5111706fyppzs2gd5br76frgvfj4x2f71";
   };
 
@@ -15,4 +15,4 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2;
     maintainers = with maintainers; [ oxzi ];
   };
-}
+})
