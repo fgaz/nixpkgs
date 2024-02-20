a689ba1623b06fdbc2dad328dc88c17828fd1855
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:25:47 +0000

    yasr: use finalAttrs

--- pkgs/applications/audio/yasr/default.nix
+++ pkgs/applications/audio/yasr/default.nix
@@ -1,12 +1,12 @@
 {lib, stdenv,fetchurl}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "yasr";
 
   version = "0.6.9";
 
   src = fetchurl {
-    url = "https://sourceforge.net/projects/yasr/files/yasr/${version}/${pname}-${version}.tar.gz";
+    url = "https://sourceforge.net/projects/yasr/files/yasr/${finalAttrs.version}/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
     sha256 = "1prv9r9y6jb5ga5578ldiw507fa414m60xhlvjl29278p3x7rwa1";
   };
 
@@ -25,4 +25,4 @@ stdenv.mkDerivation rec {
     license = lib.licenses.gpl2;
     maintainers = with lib.maintainers; [ jhhuh ];
   };
-}
+})
