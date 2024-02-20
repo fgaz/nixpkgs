3efc4c761362c709e8c11799f9f676e80fb1ada7
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:15:50 +0000

    alchemy: use finalAttrs

--- pkgs/applications/graphics/alchemy/default.nix
+++ pkgs/applications/graphics/alchemy/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchurl, jre, runtimeShell }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "alchemy";
   version = "008";
 
   enableParallelBuilding = true;
 
   src = fetchurl {
-    url = "http://al.chemy.org/files/Alchemy-${version}.tar.gz";
+    url = "http://al.chemy.org/files/Alchemy-${finalAttrs.version}.tar.gz";
     sha256 = "0449bvdccgx1jqnws1bckzs4nv2d230523qs0jx015gi81s1q7li";
   };
 
@@ -38,4 +38,4 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.marcweber ];
     platforms = platforms.linux;
   };
-}
+})
