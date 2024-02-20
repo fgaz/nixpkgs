e4c62035f86bcfb1c6dd47196d319bfa22c87eca
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:28:59 +0000

    japa: use finalAttrs

--- pkgs/applications/audio/japa/default.nix
+++ pkgs/applications/audio/japa/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl,   alsa-lib, libjack2, fftwFloat, libclthreads, libclxclient, libX11,  libXft, zita-alsa-pcmi, }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   version = "0.9.4";
   pname = "japa";
 
   src = fetchurl {
-    url = "https://kokkinizita.linuxaudio.org/linuxaudio/downloads/${pname}-${version}.tar.bz2";
+    url = "https://kokkinizita.linuxaudio.org/linuxaudio/downloads/${finalAttrs.pname}-${finalAttrs.version}.tar.bz2";
     sha256 = "sha256-t9wlZr+pE5u6yTpATWDQseC/rf4TFbtG0X9tnTdkB8I=";
   };
 
@@ -27,4 +27,4 @@ stdenv.mkDerivation rec {
     maintainers = [ lib.maintainers.magnetophon ];
     platforms = lib.platforms.linux;
   };
-}
+})
