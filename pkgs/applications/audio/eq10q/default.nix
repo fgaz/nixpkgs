0bae8cac888db1f46ce1801cbaca64f90296384d
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:30:05 +0000

    eq10q: use finalAttrs

--- pkgs/applications/audio/eq10q/default.nix
+++ pkgs/applications/audio/eq10q/default.nix
@@ -1,10 +1,10 @@
 { lib, stdenv, fetchurl, fetchpatch, cmake, fftw, gtkmm2, libxcb, lv2, pkg-config
 , xorg }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "eq10q";
   version = "2.2";
   src = fetchurl {
-    url = "mirror://sourceforge/project/eq10q/${pname}-${version}.tar.gz";
+    url = "mirror://sourceforge/project/eq10q/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
     sha256 = "16mhcav8gwkp29k9ki4dlkajlcgh1i2wvldabxb046d37dq4qzrk";
   };
 
@@ -36,9 +36,9 @@ stdenv.mkDerivation rec {
     broken = (stdenv.isLinux && stdenv.isAarch64);
     description = "LV2 EQ plugins and more, with 64 bit processing";
     longDescription = ''
-      Up to 10-Bands parametric equalizer with mono and stereo versions.
-      Versatile noise-gate plugin with mono and stereo versions.
-      Compressor plugin with mono and stereo versions.
+      Up to 10-Bands parametric equalizer with mono and stereo finalAttrs.versions.
+      Versatile noise-gate plugin with mono and stereo finalAttrs.versions.
+      Compressor plugin with mono and stereo finalAttrs.versions.
       BassUp plugin - Enhanceing the bass guitar sound or other low frequency sounding instruments.
       Improved high frequency response for peaking filter (in equalizers).
       64 bits floating point internal audio processing.
@@ -49,4 +49,4 @@ stdenv.mkDerivation rec {
     maintainers = [ lib.maintainers.magnetophon ];
     platforms = lib.platforms.linux;
   };
-}
+})
