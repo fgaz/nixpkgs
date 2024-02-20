7cf15fa67b386643adcd0992d3ea307ad89d5c32
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:44:38 +0000

    git-test: use finalAttrs

--- pkgs/applications/version-management/git-test/default.nix
+++ pkgs/applications/version-management/git-test/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, makeWrapper, git }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "git-test";
   version = "1.0.4";
 
   src = fetchFromGitHub {
     owner = "spotify";
     repo = "git-test";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "01h3f0andv1p7pwir3k6n01v92hgr5zbjadfwl144yjw9x37fm2f";
   };
 
@@ -31,4 +31,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.all;
     mainProgram = "git-test";
   };
-}
+})
