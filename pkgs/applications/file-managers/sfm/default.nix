095c60693f18c1fb3f24cf1cc511a152f07fddc8
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:11:12 +0000

    sfm: use finalAttrs

--- pkgs/applications/file-managers/sfm/default.nix
+++ pkgs/applications/file-managers/sfm/default.nix
@@ -1,19 +1,19 @@
 { lib, stdenv, fetchFromGitHub, writeText, conf ? null }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "sfm";
   version = "0.4";
 
   src = fetchFromGitHub {
     owner = "afify";
-    repo = pname;
-    rev = "v${version}";
+    repo = finalAttrs.pname;
+    rev = "v${finalAttrs.version}";
     hash = "sha256-VwPux6n+azpR4qDkzZJia95pJJOaFDBBoz6/VwlC0zw=";
   };
 
   configFile = lib.optionalString (conf!=null) (writeText "config.def.h" conf);
 
-  postPatch = lib.optionalString (conf!=null) "cp ${configFile} config.def.h";
+  postPatch = lib.optionalString (conf!=null) "cp ${finalAttrs.configFile} config.def.h";
 
   makeFlags = [ "CC:=$(CC)" ];
 
@@ -26,4 +26,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.unix;
     maintainers = with maintainers; [ sikmir ];
   };
-}
+})
