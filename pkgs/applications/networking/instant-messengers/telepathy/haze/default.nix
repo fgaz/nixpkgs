b5bd68e637c42a4716fcb6016867524ff79ad0a8
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:57:47 +0000

    telepathy-haze: use finalAttrs

--- pkgs/applications/networking/instant-messengers/telepathy/haze/default.nix
+++ pkgs/applications/networking/instant-messengers/telepathy/haze/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl, pidgin, telepathy-glib, python3, glib, dbus-glib, pkg-config, libxslt }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "telepathy-haze";
   version = "0.8.1";
 
   src = fetchurl {
-    url = "https://telepathy.freedesktop.org/releases/telepathy-haze/telepathy-haze-${version}.tar.gz";
+    url = "https://telepathy.freedesktop.org/releases/telepathy-haze/telepathy-haze-${finalAttrs.version}.tar.gz";
     hash = "sha256-cEvvpC7sIXPspLrAH/0AQBRmXyutRtyJSOVCM2TN4wo=";
   };
 
@@ -19,4 +19,4 @@ stdenv.mkDerivation rec {
     license = lib.licenses.gpl2Plus;
     platforms = lib.platforms.unix;
   };
-}
+})
