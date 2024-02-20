adb84e4bacb2a37e73036f5bdfef1135b40437df
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:04:26 +0000

    xkbmon: use finalAttrs

--- pkgs/applications/misc/xkbmon/default.nix
+++ pkgs/applications/misc/xkbmon/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, libX11 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xkbmon";
   version = "0.4";
 
   src = fetchFromGitHub {
     owner = "xkbmon";
     repo = "xkbmon";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "sha256-EWW6L6NojzXodDOET01LMcQT8/1JIMpOD++MCiM3j1Y=";
   };
 
@@ -22,4 +22,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     maintainers = [ maintainers.romildo ];
   };
-}
+})
