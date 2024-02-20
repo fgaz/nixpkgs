d5240a42b67e45fc25063dc75fe553ef2751ed00
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:39:09 +0000

    gerrit: use finalAttrs

--- pkgs/applications/version-management/gerrit/default.nix
+++ pkgs/applications/version-management/gerrit/default.nix
@@ -1,17 +1,17 @@
 { lib, stdenv, fetchurl }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gerrit";
   version = "3.9.1";
 
   src = fetchurl {
-    url = "https://gerrit-releases.storage.googleapis.com/gerrit-${version}.war";
+    url = "https://gerrit-releases.storage.googleapis.com/gerrit-${finalAttrs.version}.war";
     hash = "sha256-WQjzkykKtrXfkNSWcM9GWy8LPMwxJpSbnWjpmslP0HA=";
   };
 
   buildCommand = ''
     mkdir -p "$out"/webapps/
-    ln -s ${src} "$out"/webapps/gerrit-${version}.war
+    ln -s ${finalAttrs.src} "$out"/webapps/gerrit-${finalAttrs.version}.war
   '';
 
   passthru = {
@@ -35,9 +35,9 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     homepage = "https://www.gerritcodereview.com/index.md";
     license = licenses.asl20;
-    description = "A web based code review and repository management for the git version control system";
+    description = "A web based code review and repository management for the git finalAttrs.version control system";
     sourceProvenance = with sourceTypes; [ binaryBytecode ];
     maintainers = with maintainers; [ flokli zimbatm ];
     platforms = platforms.unix;
   };
-}
+})
