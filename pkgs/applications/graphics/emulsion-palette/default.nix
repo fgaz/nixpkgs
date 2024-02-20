1b267d51ce10605772b2bb0a4b16b74581a5903e
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:14:01 +0000

    emulsion-palette: use finalAttrs

--- pkgs/applications/graphics/emulsion-palette/default.nix
+++ pkgs/applications/graphics/emulsion-palette/default.nix
@@ -13,14 +13,14 @@
 , desktop-file-utils
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "emulsion-palette";
   version = "3.3.9";
 
   src = fetchFromGitHub {
     owner = "lainsce";
     repo = "emulsion";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "sha256-xG7yZKbbNao/pzFhdTMof/lw9K12NKZi47YRaEd65ok=";
   };
 
@@ -45,4 +45,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     maintainers = with maintainers; [ foo-dogsquared ];
   };
-}
+})
