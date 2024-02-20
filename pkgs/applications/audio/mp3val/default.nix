cbd6a50e505535b5f501e760492a64ece378293d
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:22:40 +0000

    mp3val: use finalAttrs

--- pkgs/applications/audio/mp3val/default.nix
+++ pkgs/applications/audio/mp3val/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mp3val";
   version = "0.1.8";
 
   src = fetchurl {
-    url = "mirror://sourceforge/mp3val/${pname}-${version}-src.tar.gz";
+    url = "mirror://sourceforge/mp3val/${finalAttrs.pname}-${finalAttrs.version}-src.tar.gz";
     sha256 = "17y3646ghr38r620vkrxin3dksxqig5yb3nn4cfv6arm7kz6x8cm";
   };
 
@@ -26,7 +26,7 @@ stdenv.mkDerivation rec {
       also able to fix most of the problems. Being a multiplatform application,
       MP3val can be run both under Windows and under Linux (or BSD). The most
       common MPEG audio file type is MPEG 1 Layer III (mp3), but MP3val supports
-      also other MPEG versions and layers. The tool is also aware of the most
+      also other MPEG finalAttrs.versions and layers. The tool is also aware of the most
       common types of tags (ID3v1, ID3v2, APEv2).
     '';
     homepage = "https://mp3val.sourceforge.net/index.shtml";
@@ -34,4 +34,4 @@ stdenv.mkDerivation rec {
     platforms = lib.platforms.unix;
     maintainers = [ lib.maintainers.devhell ];
   };
-}
+})
