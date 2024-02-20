8aaa49ae2d4880342fb3c2b0ca0043bad20c16c4
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:31:00 +0000

    id3v2: use finalAttrs

--- pkgs/applications/audio/id3v2/default.nix
+++ pkgs/applications/audio/id3v2/default.nix
@@ -1,11 +1,11 @@
 {lib, stdenv, fetchurl, id3lib, groff, zlib}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "id3v2";
   version = "0.1.12";
 
   src = fetchurl {
-    url = "mirror://sourceforge/id3v2/${pname}-${version}.tar.gz";
+    url = "mirror://sourceforge/id3v2/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
     sha256 = "1gr22w8gar7zh5pyyvdy7cy26i47l57jp1l1nd60xfwx339zl1c1";
   };
 
@@ -25,4 +25,4 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2Plus;
     platforms = with platforms; unix;
   };
-}
+})
