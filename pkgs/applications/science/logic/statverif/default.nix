ef1f25efbe268ef459911b8e3f4d119518671583
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:44:02 +0000

    statverif: use finalAttrs

--- pkgs/applications/science/logic/statverif/default.nix
+++ pkgs/applications/science/logic/statverif/default.nix
@@ -1,16 +1,16 @@
 { lib, stdenv, fetchurl, ocaml }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "statverif";
   version = "1.86pl4";
 
   src = fetchurl {
-    url    = "http://prosecco.gforge.inria.fr/personal/bblanche/proverif/proverif${version}.tar.gz";
+    url    = "http://prosecco.gforge.inria.fr/personal/bblanche/proverif/proverif${finalAttrs.version}.tar.gz";
     sha256 = "163vdcixs764jj8xa08w80qm4kcijf7xj911yp8jvz6pi1q5g13i";
   };
 
   pf-patch = fetchurl {
-    url    = "http://markryan.eu/research/statverif/files/proverif-${version}-statverif-2657ab4.patch";
+    url    = "http://markryan.eu/research/statverif/files/proverif-${finalAttrs.version}-statverif-2657ab4.patch";
     sha256 = "113jjhi1qkcggbsmbw8fa9ln8vs7vy2r288szks7rn0jjn0wxmbw";
   };
 
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ ocaml ];
 
-  patchPhase = "patch -p1 < ${pf-patch}";
+  patchPhase = "patch -p1 < ${finalAttrs.pf-patch}";
   buildPhase = "./build";
   installPhase = ''
     mkdir -p $out/bin
@@ -33,4 +33,4 @@ stdenv.mkDerivation rec {
     platforms   = lib.platforms.unix;
     maintainers = [ lib.maintainers.thoughtpolice ];
   };
-}
+})
