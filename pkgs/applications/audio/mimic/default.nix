989e52de8c027282e0d207381ebb5ed3d1d0fd0b
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:28:40 +0000

    mimic: use finalAttrs

--- pkgs/applications/audio/mimic/default.nix
+++ pkgs/applications/audio/mimic/default.nix
@@ -3,14 +3,14 @@
 , alsa-lib, alsa-plugins, libtool, icu, pcre2
 , pulseaudioSupport ? config.pulseaudio or false, libpulseaudio }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mimic";
   version = "1.3.0.1";
 
   src = fetchFromGitHub {
     owner = "MycroftAI";
     repo = "mimic1";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "1agwgby9ql8r3x5rd1rgx3xp9y4cdg4pi3kqlz3vanv9na8nf3id";
   };
 
@@ -55,4 +55,4 @@ stdenv.mkDerivation rec {
     platforms = lib.platforms.linux;
     maintainers = [ lib.maintainers.noneucat ];
   };
-}
+})
