1e7e140e15348c6ded6af5a0b5c7f33264d6472a
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:10:32 +0000

    blogc: use finalAttrs

--- pkgs/applications/misc/blogc/default.nix
+++ pkgs/applications/misc/blogc/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, ronn, git, cmocka }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "blogc";
   version = "0.20.1";
 
   src = fetchFromGitHub {
     owner = "blogc";
-    repo = pname;
-    rev = "v${version}";
+    repo = finalAttrs.pname;
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-YAwGgV5Vllz8JlIASbGIkdRzpciQbgPiXl5DjiSEJyE=";
   };
 
@@ -30,4 +30,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.unix;
     maintainers = with maintainers; [ sikmir ];
   };
-}
+})
