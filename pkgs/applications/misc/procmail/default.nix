970f2b239720e87a3f4741ee41ca0344e7b36483
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:06:30 +0000

    procmail: use finalAttrs

--- pkgs/applications/misc/procmail/default.nix
+++ pkgs/applications/misc/procmail/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchurl }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "procmail";
   version = "3.24";
 
   src = fetchurl {
-    url = "https://github.com/BuGlessRB/procmail/archive/refs/tags/v${version}.tar.gz";
+    url = "https://github.com/BuGlessRB/procmail/archive/refs/tags/v${finalAttrs.version}.tar.gz";
     sha256 = "UU6kMzOXg+ld+TIeeUdx5Ih7mCOsVf2yRpcCz2m9OYk=";
   };
 
@@ -29,4 +29,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.unix;
     maintainers = with maintainers; [ gebner ];
   };
-}
+})
