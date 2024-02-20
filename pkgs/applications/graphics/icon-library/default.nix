4ad82b5cb76ec2cee3c9a501b90c3f425cc11025
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:17:58 +0000

    icon-library: use finalAttrs

--- pkgs/applications/graphics/icon-library/default.nix
+++ pkgs/applications/graphics/icon-library/default.nix
@@ -17,12 +17,12 @@
 , darwin
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "icon-library";
   version = "0.0.17";
 
   src = fetchurl {
-    url = "https://gitlab.gnome.org/World/design/icon-library/uploads/8c4cad88809cd4ddc0eeae6f5170c001/icon-library-${version}.tar.xz";
+    url = "https://gitlab.gnome.org/World/design/icon-library/uploads/8c4cad88809cd4ddc0eeae6f5170c001/icon-library-${finalAttrs.version}.tar.xz";
     hash = "sha256-Gspx3fJl+ZoUN3heGWaeMuxUsjWCrIdg4pJj7DeMTSY=";
   };
 
@@ -52,4 +52,4 @@ stdenv.mkDerivation rec {
     license = licenses.gpl3Plus;
     platforms = platforms.unix;
   };
-}
+})
