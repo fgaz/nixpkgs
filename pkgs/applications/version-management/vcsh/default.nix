278a93d70fa06da375132fb2e8ab239fe219459e
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 14:38:42 +0000

    vcsh: use finalAttrs

--- pkgs/applications/version-management/vcsh/default.nix
+++ pkgs/applications/version-management/vcsh/default.nix
@@ -6,12 +6,12 @@
 , perlPackages
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "vcsh";
   version = "2.0.7";
 
   src = fetchurl {
-    url = "https://github.com/RichiH/vcsh/releases/download/v${version}/${pname}-${version}.tar.xz";
+    url = "https://github.com/RichiH/vcsh/releases/download/v${finalAttrs.version}/${finalAttrs.pname}-${finalAttrs.version}.tar.xz";
     sha256 = "sha256-Rx5yBCDRqFNyhP0Pfoo2upn7t4Yh5hxTgNKmMtaY/08=";
   };
 
@@ -30,10 +30,10 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Version Control System for $HOME";
     homepage = "https://github.com/RichiH/vcsh";
-    changelog = "https://github.com/RichiH/vcsh/blob/v${version}/changelog";
+    changelog = "https://github.com/RichiH/vcsh/blob/v${finalAttrs.version}/changelog";
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ ttuegel alerque ];
     platforms = platforms.unix;
     mainProgram = "vcsh";
   };
-}
+})
