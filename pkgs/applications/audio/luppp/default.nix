98402d999f01f4cfc9db309fa9d95be9aa5b51df
Francesco Gazzetta <fgaz@fgaz.me>
2024-02-20 15:25:46 +0000

    luppp: use finalAttrs

--- pkgs/applications/audio/luppp/default.nix
+++ pkgs/applications/audio/luppp/default.nix
@@ -11,14 +11,14 @@
 , ntk
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "luppp";
   version = "1.2.1";
 
   src = fetchFromGitHub {
     owner = "openAVproductions";
     repo = "openAV-Luppp";
-    rev = "release-${version}";
+    rev = "release-${finalAttrs.version}";
     sha256 = "1ncbn099fyfnr7jw2bp3wf2g9k738lw53m6ssw6wji2wxwmghv78";
   };
 
@@ -44,4 +44,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ prusnak ];
     platforms = platforms.linux;
   };
-}
+})
