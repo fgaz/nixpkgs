e175b8a673bb496a39d8959a9a20575bdfe6a164
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 08:37:49 +0000

    lv2-cpp-tools: use finalAttrs pattern

--- pkgs/applications/audio/lv2-cpp-tools/default.nix
+++ pkgs/applications/audio/lv2-cpp-tools/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, fetchzip, pkg-config, lv2, gtkmm2, boost }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "lv2-cpp-tools";
   version = "1.0.5";
 
   src = fetchzip {
-    url = "http://deb.debian.org/debian/pool/main/l/lv2-c++-tools/lv2-c++-tools_${version}.orig.tar.bz2";
+    url = "http://deb.debian.org/debian/pool/main/l/lv2-c++-tools/lv2-c++-tools_${finalAttrs.version}.orig.tar.bz2";
     sha256 = "039bq7d7s2bhfcnlsfq0mqxr9a9iqwg5bwcpxfi24c6yl6krydsi";
   };
 
@@ -25,4 +25,4 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.michalrus ];
     platforms = platforms.linux;
   };
-}
+})
