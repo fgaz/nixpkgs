94feea86528de9fd51aa0887167c96d47bd1f5f1
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:02:35 +0000

    xkblayout-state: use finalAttrs pattern

--- pkgs/applications/misc/xkblayout-state/default.nix
+++ pkgs/applications/misc/xkblayout-state/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, libX11 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xkblayout-state";
   version = "1b";
 
   src = fetchFromGitHub {
     owner = "nonpop";
     repo = "xkblayout-state";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-diorqwDEBdzcBteKvhRisQaY3bx5seaOaWSaPwBkWDo=";
   };
 
@@ -25,4 +25,4 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.jagajaga ];
     platforms = platforms.linux;
   };
-}
+})
