ed922ca538f21a7757383095db619112246b9305
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:03:20 +0000

    gmni: use finalAttrs pattern

--- pkgs/applications/networking/browsers/gmni/default.nix
+++ pkgs/applications/networking/browsers/gmni/default.nix
@@ -1,13 +1,13 @@
 { stdenv, lib, fetchFromSourcehut, bearssl, scdoc }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gmni";
   version = "1.0";
 
   src = fetchFromSourcehut {
     owner = "~sircmpwn";
     repo = "gmni";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "sha256-3MFNAI/SfFigNfitfFs3o9kkz7JeEflMHiH7iJpLfi4=";
   };
 
@@ -21,4 +21,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ bsima jb55 ];
     platforms = platforms.linux;
   };
-}
+})
