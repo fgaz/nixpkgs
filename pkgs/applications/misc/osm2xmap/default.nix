16ded59fc214a8b66ad9eb16e88bf727d6ac8565
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 08:57:58 +0000

    osm2xmap: use finalAttrs pattern

--- pkgs/applications/misc/osm2xmap/default.nix
+++ pkgs/applications/misc/osm2xmap/default.nix
@@ -1,6 +1,6 @@
 { lib, stdenv, fetchFromGitHub, libroxml, proj_7, yaml-cpp_0_3, boost } :
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "osm2xmap";
   version = "2.0";
 
@@ -8,11 +8,11 @@ stdenv.mkDerivation rec {
     sha256 = "1d3f18wzk240yp0q8i2vskhcfj5ar61s4hw83vgps0wr2aglph3w";
     repo = "osm2xmap";
     owner = "sembruk";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
   };
 
   makeFlags = [
-    "GIT_VERSION=${version}"
+    "GIT_VERSION=${finalAttrs.version}"
     "GIT_TIMESTAMP="
     "SHAREDIR=${placeholder "out"}/share/osm2xmap/"
     "INSTALL_BINDIR=${placeholder "out"}/bin"
@@ -30,4 +30,4 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.mpickering ];
     platforms = with lib.platforms; linux;
   };
-}
+})
