97c4bd43daba074b5d63a4187fe7f10501c176c3
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:26:25 +0000

    bspwm: use finalAttrs pattern

--- pkgs/applications/window-managers/bspwm/default.nix
+++ pkgs/applications/window-managers/bspwm/default.nix
@@ -2,14 +2,14 @@
 , xcbutil, xcbutilkeysyms, xcbutilwm
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "bspwm";
   version = "0.9.10";
 
   src = fetchFromGitHub {
     owner  = "baskerville";
     repo   = "bspwm";
-    rev    = version;
+    rev    = finalAttrs.version;
     sha256 = "0qlv7b4c2mmjfd65y100d11x8iqyg5f6lfiws3cgmpjidhdygnxc";
   };
 
@@ -24,4 +24,4 @@ stdenv.mkDerivation rec {
     license = licenses.bsd2;
     platforms = platforms.linux;
   };
-}
+})
