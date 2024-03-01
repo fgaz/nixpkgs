e6d8aef21145a46c78bdb837721a8772d6eb0f7f
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:09:28 +0000

    openrsync: use finalAttrs pattern

--- pkgs/applications/networking/sync/openrsync/default.nix
+++ pkgs/applications/networking/sync/openrsync/default.nix
@@ -3,7 +3,7 @@
 , fetchFromGitHub
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "openrsync";
   version = "unstable-2022-05-08";
 
@@ -26,4 +26,4 @@ stdenv.mkDerivation rec {
     # https://github.com/kristapsdz/oconfigure#readme
     platforms = platforms.unix;
   };
-}
+})
