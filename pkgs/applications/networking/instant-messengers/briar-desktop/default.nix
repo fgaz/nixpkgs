e30f86458fc638d2d90c7773371ca1a17ac7cbc4
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:04:50 +0000

    briar-desktop: use finalAttrs pattern

--- pkgs/applications/networking/instant-messengers/briar-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/briar-desktop/default.nix
@@ -17,12 +17,12 @@ let
   '';
 
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "briar-desktop";
   version = "0.5.0-beta";
 
   src = fetchurl {
-    url = "https://desktop.briarproject.org/jars/linux/${version}/briar-desktop-linux-${version}.jar";
+    url = "https://desktop.briarproject.org/jars/linux/${finalAttrs.version}/briar-desktop-linux-${finalAttrs.version}.jar";
     hash = "sha256-J93ODYAiRbQxG2BF7P3H792ymAcJ3f07l7zRSw8kM+E=";
   };
 
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/{bin,lib}
-    cp ${src} $out/lib/briar-desktop.jar
+    cp ${finalAttrs.src} $out/lib/briar-desktop.jar
     makeWrapper ${openjdk}/bin/java $out/bin/briar-desktop \
       --add-flags "-jar $out/lib/briar-desktop.jar" \
       --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [
@@ -60,4 +60,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ onny ];
     platforms = [ "x86_64-linux" ];
   };
-}
+})
