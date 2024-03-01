7a591725e55dce0315f428e57817f43bebffb650
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:02:52 +0000

    xsw: use finalAttrs pattern

--- pkgs/applications/misc/xsw/default.nix
+++ pkgs/applications/misc/xsw/default.nix
@@ -3,14 +3,14 @@
 let
   makeSDLFlags = map (p: "-I${lib.getDev p}/include/SDL");
 
-in stdenv.mkDerivation rec {
+in stdenv.mkDerivation (finalAttrs: {
   pname = "xsw";
   version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "andrenho";
     repo = "xsw";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "092vp61ngd2vscsvyisi7dv6qrk5m1i81gg19hyfl5qvjq5p0p8g";
   };
 
@@ -25,11 +25,11 @@ in stdenv.mkDerivation rec {
   ];
 
   meta = with lib; {
-    inherit (src.meta) homepage;
+    inherit (finalAttrs.src.meta) homepage;
     description = "A slide show presentation tool";
 
     platforms = platforms.unix;
     license  = licenses.gpl3;
     maintainers = [ maintainers.vrthra ];
   };
-}
+})
