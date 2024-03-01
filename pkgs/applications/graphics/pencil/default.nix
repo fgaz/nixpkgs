3287361eb85fd72bcd75581f328da96f051b5136
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 08:50:22 +0000

    pencil: use finalAttrs pattern

--- pkgs/applications/graphics/pencil/default.nix
+++ pkgs/applications/graphics/pencil/default.nix
@@ -41,12 +41,12 @@ let
     stdenv.cc.cc
   ];
 
-in stdenv.mkDerivation rec {
+in stdenv.mkDerivation (finalAttrs: {
   version = "3.1.0";
   pname = "pencil";
 
   src = fetchurl {
-    url    = "http://pencil.evolus.vn/dl/V${version}.ga/pencil_${version}.ga_amd64.deb";
+    url    = "http://pencil.evolus.vn/dl/V${finalAttrs.version}.ga/pencil_${finalAttrs.version}.ga_amd64.deb";
     sha256 = "01ae54b1a1351b909eb2366c6ec00816e1deba370e58f35601cf7368f10aaba3";
   };
 
@@ -104,4 +104,4 @@ in stdenv.mkDerivation rec {
     maintainers = with maintainers; [ bjornfor prikhi mrVanDalo ];
     platforms   = platforms.linux;
   };
-}
+})
