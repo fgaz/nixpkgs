8db3d4c3f0ff15ca3d16e8b1706e2d02b72229c3
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:00:03 +0000

    siglo: use finalAttrs pattern

--- pkgs/applications/misc/siglo/default.nix
+++ pkgs/applications/misc/siglo/default.nix
@@ -11,14 +11,14 @@
 , python3
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "siglo";
   version = "0.9.9";
 
   src = fetchFromGitHub {
     owner = "theironrobin";
     repo = "siglo";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-4jKsRpzuyHH31LXndC3Ua4TYcI0G0v9qqe0cbvLuCDA=";
   };
 
@@ -67,4 +67,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ ];
     platforms = platforms.linux;
   };
-}
+})
