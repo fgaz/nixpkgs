2438b873974abceda258d9dff9e361783f330a32
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:01:14 +0000

    tnef: use finalAttrs pattern

--- pkgs/applications/misc/tnef/default.nix
+++ pkgs/applications/misc/tnef/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, autoreconfHook }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   version = "1.4.18";
   pname = "tnef";
 
   src = fetchFromGitHub {
     owner  = "verdammelt";
     repo   = "tnef";
-    rev    = version;
+    rev    = finalAttrs.version;
     sha256 = "104g48mcm00bgiyzas2vf86331w7bnw7h3bc11ib4lp7rz6zqfck";
   };
 
@@ -29,4 +29,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ peterhoeg ];
     platforms = platforms.all;
   };
-}
+})
