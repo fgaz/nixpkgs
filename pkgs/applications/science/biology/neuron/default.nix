fddf528759015481449a461c24b88609e0fd584d
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:14:29 +0000

    neuron: use finalAttrs pattern

--- pkgs/applications/science/biology/neuron/default.nix
+++ pkgs/applications/science/biology/neuron/default.nix
@@ -19,7 +19,7 @@
 }:
 
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "neuron";
   version = "8.2.3";
 
@@ -82,7 +82,7 @@ stdenv.mkDerivation rec {
   '';
 
   src = fetchurl {
-    url = "https://github.com/neuronsimulator/nrn/releases/download/${version}/full-src-package-${version}.tar.gz";
+    url = "https://github.com/neuronsimulator/nrn/releases/download/${finalAttrs.version}/full-src-package-${finalAttrs.version}.tar.gz";
     sha256 = "sha256-k8+71BRfh+a73sZho6v0QFRxVmrfx6jqrgaqammdtDI=";
   };
 
@@ -101,4 +101,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ adev davidcromp ];
     platforms = platforms.all;
   };
-}
+})
