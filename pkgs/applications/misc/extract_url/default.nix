46741703220a2d6b94f33c5a20d06f02003cc0ad
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 08:53:29 +0000

    extract_url: use finalAttrs pattern

--- pkgs/applications/misc/extract_url/default.nix
+++ pkgs/applications/misc/extract_url/default.nix
@@ -9,14 +9,14 @@ let
     ++ lib.optional cursesSupport perlPackages.CursesUI
     ++ lib.optional uriFindSupport perlPackages.URIFind;
 
-in stdenv.mkDerivation rec {
+in stdenv.mkDerivation (finalAttrs: {
   pname = "extract_url";
   version = "1.6.2";
 
   src = fetchFromGitHub {
     owner = "m3m0ryh0l3";
     repo = "extracturl";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "05589lp15jmcpbj4y9a3hmf6n2gsqrm4ybcyh3hd4j6pc7hmnhny";
   };
 
@@ -38,4 +38,4 @@ in stdenv.mkDerivation rec {
     maintainers = [ maintainers.qyliss ];
     platforms = platforms.unix;
   };
-}
+})
