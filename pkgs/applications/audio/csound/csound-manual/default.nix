4a014abe6363b974ba5c208bc0f407b1ed05afc3
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 08:35:20 +0000

    csound-manual: use finalAttrs pattern

--- pkgs/applications/audio/csound/csound-manual/default.nix
+++ pkgs/applications/audio/csound/csound-manual/default.nix
@@ -7,14 +7,14 @@
 , libxslt
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "csound-manual";
   version = "6.18.0";
 
   src = fetchFromGitHub {
     owner = "csound";
     repo = "manual";
-    rev = version;
+    rev = finalAttrs.version;
     sha256 = "sha256-W8MghqUBr3V7LPgNwU6Ugw16wdK3G37zAPuasMlZ2+I=";
   };
 
@@ -42,4 +42,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ hlolli ];
     platforms = lib.platforms.all;
   };
-}
+})
