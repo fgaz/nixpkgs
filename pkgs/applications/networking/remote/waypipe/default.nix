97ddd1f656f3e574938c2a06541379bc2ce7cffa
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 09:08:53 +0000

    waypipe: use finalAttrs pattern

--- pkgs/applications/networking/remote/waypipe/default.nix
+++ pkgs/applications/networking/remote/waypipe/default.nix
@@ -3,7 +3,7 @@
 , mesa, lz4, zstd, ffmpeg, libva
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "waypipe";
   version = "0.8.6";
 
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     domain = "gitlab.freedesktop.org";
     owner = "mstoeckl";
     repo = "waypipe";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-1VLPnP4BmF9Zha0uVsPjA/WbF/oLfZmdDX57SzqrV5A=";
   };
 
@@ -36,4 +36,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     maintainers = with maintainers; [ primeos ];
   };
-}
+})
