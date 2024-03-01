b930bc4b4d267a33b23bd895fe1ecfc805d0d04c
Francesco Gazzetta <fgaz@fgaz.me>
2024-03-01 08:36:15 +0000

    freac: use finalAttrs pattern

--- pkgs/applications/audio/freac/default.nix
+++ pkgs/applications/audio/freac/default.nix
@@ -7,14 +7,14 @@
 , systemd
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "freac";
   version = "1.1.7";
 
   src = fetchFromGitHub {
     owner = "enzo1982";
     repo = "freac";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-bHoRxxhSM7ipRkiBG7hEa1Iw8Z3tOHQ/atngC/3X1a4=";
   };
 
@@ -35,4 +35,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ shamilton ];
     platforms = platforms.linux;
   };
-}
+})
